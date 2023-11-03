8463d60e90e3ba59b0db012d202a56b1e4bf12b8
cleiton lahm <cleijllahm@gmail.com>
2023-11-03 10:37:50 -0300

    criando o componente Section e construindo a secao detalhes

--- src/components/Section/styles.ts
+++ src/components/Section/styles.ts
@@ -13,12 +13,12 @@ export const Container = styled.section<Omit<Props, 'title' | 'games'>>`
     background-color: ${(props) =>
       props.background === 'black' ? cores.cinza : cores.preto};
   }
-`
-export const List = styled.ul`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  column-gap: 24px;
-  margin-top: 40px;
+
+  p {
+    font-size: 14px;
+    line-height: 22px;
+    max-width: 640px;
+  }
 `
 export const Title = styled.h2`
   font-size: 18px;
--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom'
 import Hero from '../../components/Hero'
+import Section from '../../components/Section'
 
 const Product = () => {
   const { id } = useParams()
@@ -7,6 +8,28 @@ const Product = () => {
   return (
     <>
       <Hero />
+      <Section title="Sobre o jogo" background="black">
+        <p>
+          Hogwarts Legacy é um RPG de ação no universo de Harry Potter.
+          Desenvolvido pela Portkey Games, leva os jogadores à Hogwarts do
+          século XIX. Eles criam um personagem, escolhem uma casa e exploram um
+          mundo mágico com missões, magias e escolhas que moldam a história. Os
+          jogadores podem reviver a magia de Hogwarts, desvendar segredos e
+          enfrentar desafios mágicos neste jogo envolvente.
+        </p>
+      </Section>
+      <Section title="Mais detalhes" background="gray">
+        <p>
+          <b>Plataforma:</b> Disponível em várias plataformas, incluindo PC,
+          PlayStation e Xbox. <br /> <b>Desenvolvedor:</b> Portkey Games <br />
+          <b>Editora:</b> Warner Bros. Interactive Entertainment <br />
+          <b>Idiomas:</b> O jogo oferece suporte a vários idiomas, incluindo
+          inglês, espanhol, francês, entre outros.
+        </p>
+      </Section>
+      <Section title="Galeria" background="black">
+        <div>fotos</div>
+      </Section>
     </>
   )
 }
