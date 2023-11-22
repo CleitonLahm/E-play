04766e14ad6a570cd9694b21d7606b3560e6ea37
cleiton lahm <cleijllahm@gmail.com>
2023-11-22 18:18:16 -0300

    criando as opcoes de pagamento

--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -9,10 +9,15 @@ type RowProps = {
   marginTop?: string
 }
 
+type TabButtonProps = {
+  isActive: boolean
+}
+
 export const Row = styled.div<RowProps>`
   display: flex;
   gap: 24px;
   margin-top: ${(props) => props.marginTop || '0'};
+  align-items: flex-end;
 `
 export const InputGroup = styled.div<InputGroupProps>`
   flex: auto;
@@ -25,11 +30,29 @@ export const InputGroup = styled.div<InputGroupProps>`
     display: block;
   }
 
-  input {
+  input,
+  select {
     background-color: ${cores.branca};
     height: 32px;
     padding: 0 8px;
     border: 1px solid ${cores.branca};
     width: 100%;
+  }
+`
+export const TabButton = styled.button<TabButtonProps>`
+  border-radius: 8px;
+  font-size: 14px;
+  font-weight: bold;
+  color: ${cores.branca};
+  background-color: ${(props) => (props.isActive ? cores.verde : cores.preto)};
+  height: 32px;
+  border: none;
+  margin-right: 16px;
+  padding: 0 8px;
+  cursor: pointer;
+
+  img {
+    max-width: 16px;
+    margin-right: 8px;
   }
 `
